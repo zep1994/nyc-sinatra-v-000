03fe650d76eefd7a8a9054e46cd359b4ca327096
Thomas Wesley Matlock <snaxbags@protonmail.com>
2018-09-07 22:54:45 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -23,5 +23,58 @@ class LandmarksController < ApplicationController
     erb:'landmarks/edit'
   end
   
-  
+  post '/landmarks' do
+    @landmark = Landmark.create(name: params[:landmark][:name])
+    if !params[:landmark][:year_completed].empty?
+      @landmark.year_completed = params[:landmark][:year_completed]
+    end
+    if !params[:landmark][:name].empty?
+      landmark = Landmark.find_or_create_by(name: params[:landmark][:name])
+      @landmark.landmarks << landmark
+    end
+  end
+  if !params[:title].empty?
+    params[:figure][:title_ids] = [] if !params[:figure][:title_ids])
+      if !params[:title][:name].split(",").map!(&:strip).each do |name|
+        title = Title.find_or_create_by(name: name)
+        params[:figure][:title_ids] << title.id
+      end
+  end
+    params[:figure][:title_ids].each do |title_id|
+      @landmark.figure_titles.create(title_id: title_id)
+    end
+  end
+  @landmark.save
+  redirect :"figures/#{@figure.id}"
+
+  post '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    if !params[:figure][:name].empty?
+      @figure.name = params[:figure][:name]
+    end
+    if params[:figure][:landmarks_ids]
+      @figure.landmarks_ids = params[:figure][:landmarks_ids]
+    end
+    if !params[:landmark][:name].empty?
+      landmark = Landmark.find_or_create_by(name: params[:landmark][:name])
+      @figure.landmarks << landmark
+    end
+    if !params[:title].empty?
+      params[:figure][:title_id] = [] if !params[:figure][:title_ids]
+      if !params[:title][:name].empty?
+        params[:title][:name].split(",").map!(&:strip).each do |name|
+          title = Title.find_or_create_by(name: name)
+          params[:figure][:title_id] << title.id
+        end
+      end
+      (@figure.title_ids - params[:figure][:title_ids]).each do |title_id|
+        @figure.figure_titles.find_by(title_id: title_id).destroy
+      end
+      (params[:figure][:title_ids] - @figure.title_ids).each do |title_id|
+        @figure.figure_titles.create(title_id: title_id)
+      end
+    end
+    @figure.save
+    redirect :"figures/#{@figure.id}"
+  end
 end
