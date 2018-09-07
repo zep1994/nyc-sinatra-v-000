73e5a50b80bb7b923e12c98ba80791035df37a5b
Thomas Wesley Matlock <snaxbags@protonmail.com>
2018-09-07 17:34:47 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,4 +1,27 @@
 class FiguresController < ApplicationController
-  # add controller methods
+
+  get '/figures' do
+    @figures = Figure.all 
+    erb:'figure/index'
+  end
+  
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all 
+    erb:'figures/new'
+  end
+  
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb:'figures/show'
+  end
+  
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    @landmarks = Landmark.all
+    @titles = Title.all 
+    erb:'figures/edit'
+  end
+  
   
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,27 @@
 class LandmarksController < ApplicationController
-  # add controller methods
+ 
+  get '/landmarks' do
+   @landmarks = Landmark.all 
+   erb:'landmarks/index'
+  end
+ 
+  get '/landmarks/new' do
+   @titles = Title.all
+   @figure = Figure.all 
+   erb:'landmarks/new'
+  end
+  
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb:'landmark/show' 
+  end 
+  
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    @figures = Figure.all
+    @titles = Title.all 
+    erb:'landmarks/edit'
+  end
+  
+  
 end
