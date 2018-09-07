4fb9943764622f632f6e26fad9144544674dd038
Thomas Wesley Matlock <snaxbags@protonmail.com>
2018-09-07 22:35:06 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -23,5 +23,26 @@ class FiguresController < ApplicationController
     erb:'figures/edit'
   end
   
+  post '/figures' do
+    @figure = Figure.create(name: params[:figure][:name])
+    if params[:figure][:landmarks_ids]
+      @figure.landmarks_ids = params[:figure][:landmarks_ids]
+    end
+    if !params[:landmark][:name].empty?
+      landmark = Landmark.find_or_create_by(name: params[:landmark][:name])
+      @figure.landmarks << landmark
+    end
+  end
+  if !params[:title].empty?
+    params[:figure][:title_ids] = [] if !params[:figure][:title_ids])
+      if !params[:title][:name].split(",").map!(&:strip).each do |name|
+        title = Title.find_or_create_by(name: name)
+        params[:figure][:title_ids] << title.id 
+      end
+  end
+    params[:figure][:title_ids].each do |title_id|
+      @figure.figure_titles.create(title_id: title_id)
+    end
+  end
   
 end
